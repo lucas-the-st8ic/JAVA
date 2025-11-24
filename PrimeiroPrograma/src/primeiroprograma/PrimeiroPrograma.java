3aee631a2d85343def3f2c9099d4abc1788f5309
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-24 16:27:44 -0300

    Update PrimeiroPrograma.java

--- PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -3,7 +3,5 @@ package primeiroprograma;
 public class PrimeiroPrograma {
     static void main(String[] args) {
         System.out.println("Olá Mundo!!!");
-
-        String nome = "Lucas";
     }
 }
